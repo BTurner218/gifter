e9b27c41b80f7f0aa885df3f76807c45218d70c9
Brandon Turner <bturner218@outlook.com>
2022-06-01 21:45:17 -0400

    feat(api): connect to PostgreSQL database

--- api/main.go
+++ api/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v4"
 )
 
 func main() {
+	// TODO: Use environment variable for database URL
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres:example@localhost:5432/gifter")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Get("/", hello)
